6a5bf593e9e9cf88eef023d3c269ac73622ea706
lybo <lyberis.g@gmail.com>
2019-02-08 14:54:47 +0100

    first commit

---
+++ build/precache-manifest.b487abc231b147fca5b70ceefb4999c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f102d2de3b7f115d861b",
+    "url": "/static/js/main.f102d2de.chunk.js"
+  },
+  {
+    "revision": "ddaddb9daa6541406b82",
+    "url": "/static/js/1.ddaddb9d.chunk.js"
+  },
+  {
+    "revision": "f102d2de3b7f115d861b",
+    "url": "/static/css/main.577936f9.chunk.css"
+  },
+  {
+    "revision": "200e50c6e449c845e7e3ba9f0928c8cd",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import request from 'request-promise-native';
 import './App.css';
+// import bg from './bg.jpg';
 
 class App extends Component {
+  state = {
+    userInfo: false,
+    users: [],
+    books: [],
+  }
+
+  async componentWillMount() {
+    const users = await request({
+      method: 'GET',
+      uri: 'https://api.myjson.com/bins/18wtzc',
+    });
+    this.setState({
+      users: JSON.parse(users),
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div
+        className="App"
+      >
+        {!this.state.userInfo && (
+          <div
+            className="login"
           >
-            Learn React
-          </a>
-        </header>
+            <h2
+              className="login__title"
+            >
+              Books
+            </h2>
+            <form
+              className="login__form"
+              onSubmit={async (e) => {
+                e.preventDefault();
+                const userInfo = this.state.users.find(user => user.username === this.username.value);
+
+                if (!userInfo || !userInfo.uri) {
+                  return;
+                }
+
+                const books = await request({
+                  method: 'GET',
+                  uri: userInfo.uri,
+                });
+                this.setState({
+                  userInfo,
+                  books: JSON.parse(books),
+                });
+              }}
+            >
+              <input
+                id="username"
+                type="text"
+                name="username"
+                defaultValue=""
+                className="login__input"
+                placeholder="Username"
+                ref={(username) => this.username = username}
+              />
+              <button
+                className="login__button"
+              >
+                Load books
+              </button>
+            </form>
+          </div>
+        )}
+        {this.state.userInfo && (
+          <div>
+            <div
+              className="user-info"
+            >
+              <div
+                className="user-info__welcome"
+              >
+                hi, {this.state.userInfo.username}
+              </div>
+              <button
+                className="user-info__logout"
+                onClick={() => {
+                  this.setState({
+                    userInfo: false,
+                    books: [],
+                  });
+                }}
+              >
+                load another user
+              </button>
+            </div>
+
+            <div
+              className="books"
+            >
+              {this.state.books.map(book => (
+                <div
+                  key={book.link}
+                  className="book"
+                >
+                  <div
+                    className="book_image-container"
+                  >
+                    <img
+                      className="book__image"
+                      src={book.img}
+                      alt={book.title}
+                    />
+                  </div>
+                  <div
+                    className="book__content"
+                  >
+                    <h3
+                      className="book__title"
+                    >
+                      {book.title}
+                    </h3>
+                    <a
+                      className="book__link"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={book.link}
+                    >
+                      visit
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+          </div>
+        )}
       </div>
     );
   }
